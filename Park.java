2a17963934aaa78627425b36ee157792ea98e2c3
NassarHQ <mnassar.personal@gmail.com>
2024-09-26 12:06:32 -0700

    Add Park.java class

--- Park.java
+++ Park.java
@@ -14,6 +14,9 @@ public class Park {
     // ArrayLists to store all employees in the park
     private ArrayList<Employee> parkEmployees;
 
+    // ArrayList to store all park sections in the park
+    private ArrayList<Section> parkSections;
+
     // ArrayLists to store all rides in the park
     private ArrayList<Ride> parkRides;
 
@@ -22,14 +25,40 @@ public class Park {
 
     // Constructor for the Park class
     public Park(String parkName, String parkLocation) {
-        
+
         this.parkName = parkName; // Sets the park's name
         this.parkLocation = parkLocation; // Sets the park's location
 
         // Initialize the ArrayLists
         this.parkVisitors = new ArrayList<>();
         this.parkEmployees = new ArrayList<>();
+        this.parkSection = new ArrayList<>();
         this.parkRides = new ArrayList<>();
         this.parkTickets = new ArrayList<>();
+    }
+
+    // Method to add a visitior
+    public void addVisitor(Visitor v) {
+
+    }
+
+    // Method to remove a visitor
+    public void removeVisitor(Visitor v) {
+
+    }
+
+    // Method to assign an employee for a section
+    public void assignEmployee(Employee parkEmployees, Section parkSections) {
+
+    }
+
+    // Method to operate rides
+    public void operateRide() {
+
+    }
+
+    // Method to manage tickets in the park
+    public void manageTickets(Ticket ticket) {
+        
     }
 }
