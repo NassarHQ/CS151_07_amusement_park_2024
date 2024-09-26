1b98a7f31b9c3f8eaf0afb30bfda48eb3c2ab5b2
NassarHQ <mnassar.personal@gmail.com>
2024-09-26 14:39:00 -0700

    Updated Visitor class with new methods and modified Park class

--- Park.java
+++ Park.java
@@ -1,4 +1,4 @@
-// Imports
+// Imported Libraries
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -6,33 +6,23 @@ import java.util.HashSet;
 public class Park {
 
     // Instance variables holding park's name and location
-    private String parkName; // Name of the Park
-    private String parkLocation; // Location of the Park
+    private String parkName = "Ride & Seek"; // Name of the Park
+    private String parkLocation = "666 Roller Coaster Avenue"; // Location of the Park
 
-    // ArrayLists to store all visitors in the park
+    // HashSets to store visitors and employees.
+    // ArrayLists to store sections, rides, and tickets
     private HashSet<Visitor> visitors;
-
-    // ArrayLists to store all employees in the park
-    private ArrayList<Employee> employees;
-
-    // ArrayList to store all park sections in the park
+    private HashSet<Employee> employees;
     private ArrayList<Section> sections;
-
-    // ArrayLists to store all rides in the park
     private ArrayList<Ride> rides;
-
-    // ArrayLists to store all tickets issued in the park
     private ArrayList<Ticket> tickets;
 
     // Constructor for the Park class
-    public Park(String name, String location) {
-
-        this.parkName = parkName; // Sets the park's name
-        this.parkLocation = parkLocation; // Sets the park's location
+    public Park() {
 
         // Initialize the ArrayLists
         this.visitors = new HashSet<>();
-        this.employees = new ArrayList<>();
+        this.employees = new HashSet<>();
         this.sections = new ArrayList<>();
         this.rides = new ArrayList<>();
         this.tickets = new ArrayList<>();
@@ -41,34 +31,81 @@ public class Park {
     // Method to add a Person to the park
     public void addPerson(Person p) {
 
-        // Check if the object of person is null
+        // Check if the object of Person is null
         if (p == null) {
+            // Print an error message if object p is null
             System.out.println("Invalid person. Cannot add null");
-            return;
         }
-
-        // Check if person is a visitor
-        if (p instanceof Visitor) {
-            Visitor v = (Visitor) p;
-            if (visitors.add(v)) {
-                System.out.println("Visitor: " + v.getName() + ", added to the park");
-            } else {
-                System.out.println("Visitor: " + v.getName() + ", already in the park");
-            }
-        }
-
-        // Check if persin is an employee
-        if (p instanceof Employee) {
-            Employee e = (Employee) p;
-            if (employees.add(e)) {
-                System.out.println("Employee: " + e.getName() + ", ID: "+ e.getID + ", added to the park");
-            } else {
-                System.out.println("Employee: " + e.getName() + ", ID: "+ e.getID + ", already exists in the park");
-            }
-        } else {
-            System.out.println("Person is neither a Visitor nor an Employee");
-        }
+        p.addToPark(this);
     }
+
+    // Method to remove a Person to the park
+    public void removePeson(Person p) {
+
+        // Check if the object of Person is null
+        if (p == null) {
+            // Print an error message if object p is null
+            System.out.println("Invalid person. Cannot add null");
+        }
+        p.removeFromPark(this);
+    }
+
+    // Getter for visitors
+    public HashSet<Visitor> getVisitors() {
+        return visitors;
+    }
+
+    // Getter for employees
+    public HashSet<Employee> getEmployees() {
+        return employees;
+    }
+
+    // // Method to add a Person to the park takes object of Person
+    // public void addPerson(Person p) {
+
+    //     // Check if the object of person is null
+    //     if (p == null) {
+    //         // Print an error message if object p is null
+    //         System.out.println("Invalid person. Cannot add null");
+    //         return; // Exit the method early since there is nothing to add
+    //     }
+
+    //     // Check if person is a visitor
+    //     if (p instanceof Visitor) {
+    //         Visitor v = (Visitor) p; // Cast the Person object to a Visitor type
+
+    //         // Try to add the visitor to the HashSet (Prevents Duplicates Automatically)
+    //         if (visitors.add(v)) {
+    //             // Print a confirmation message if visitor is added
+    //             System.out.println("Visitor: " + v.getName() + ", added to the park");
+    //         } else {
+    //             // Print a failure message if visitor already exists in the HashSet
+    //             System.out.println("Visitor: " + v.getName() + ", already in the park");
+    //         }
+    //     } 
+        
+    //     // Check if person is an employee
+    //     else if (p instanceof Employee) { 
+    //         Employee e = (Employee) p; // Cast the Person object to an Employee type
+
+    //         // Try to add the employee to the HashSet (Prevents Duplicates Automatically)
+    //         if (employees.add(e)) {
+    //             // Print a confirmation message if employee is added
+    //             System.out.println("Employee: " + e.getName() + ", ID: "+ e.getID + ", added to the park");
+    //         } else {
+    //             // Print a failure message if employee already exists in the HashSet
+    //             System.out.println("Employee: " + e.getName() + ", ID: "+ e.getID + ", already exists in the park");
+    //         }
+    //     } else {
+    //         // If the person is neither a Visitor nor an Employee, print an error message
+    //         System.out.println("Person is neither a Visitor nor an Employee");
+    //     }
+    // }
+
+    // // Method to remove a person from the Park takes object of Person
+    // public void removePerson(Person p) {
+        
+    // }
 
     // Method to assign an employee for a section
     public void assignEmployee(Employee e, Section s) {
--- Visitor.java
+++ Visitor.java
@@ -10,6 +10,25 @@ public class Visitor extends Person {
 
     //Most functionality of visitors will include stuff involving TICKETS and RIDES.
 
+    @Override
+    public void addToPark(Park park) {
+        // Add visitor to the park's visitor set
+        if (park.getVisitors().add(this)) {
+            System.out.println("Visitor: " + getName() + " added to the park");
+        } else {
+            System.out.println("Visitor: " + getName() + " already in the park");
+        }
+    }
+
+    @Override
+    public void removeFromPark(Park park) {
+        // Remove the visitor from the park's visitor set
+        if (park.getVisitors().remove(this)) {
+            System.out.println("Visitor: " + getName() + " removed from the park.");
+        } else {
+            System.out.println("Visitor: " + getName() + " not found in the park.");
+        }
+    }
 
     @Override
     public void personType(){
