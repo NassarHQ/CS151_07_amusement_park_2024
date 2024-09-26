be3f36efe4eaf09eee17f9d855a1d3b6bcec30cf
NassarHQ <mnassar.personal@gmail.com>
2024-09-26 12:55:43 -0700

    Implemented functionalities of addVisitor method. Adjusted variable names for confusion

--- Park.java
+++ Park.java
@@ -9,37 +9,46 @@ public class Park {
     private String parkLocation; // Location of the Park
 
     // ArrayLists to store all visitors in the park
-    private ArrayList<Visitor> parkVisitors;
+    private ArrayList<Visitor> visitors;
 
     // ArrayLists to store all employees in the park
-    private ArrayList<Employee> parkEmployees;
+    private ArrayList<Employee> employees;
 
     // ArrayList to store all park sections in the park
-    private ArrayList<Section> parkSections;
+    private ArrayList<Section> sections;
 
     // ArrayLists to store all rides in the park
-    private ArrayList<Ride> parkRides;
+    private ArrayList<Ride> rides;
 
     // ArrayLists to store all tickets issued in the park
-    private ArrayList<Ticket> parkTickets;
+    private ArrayList<Ticket> tickets;
 
     // Constructor for the Park class
-    public Park(String parkName, String parkLocation) {
+    public Park(String name, String location) {
 
         this.parkName = parkName; // Sets the park's name
         this.parkLocation = parkLocation; // Sets the park's location
 
         // Initialize the ArrayLists
-        this.parkVisitors = new ArrayList<>();
-        this.parkEmployees = new ArrayList<>();
-        this.parkSection = new ArrayList<>();
-        this.parkRides = new ArrayList<>();
-        this.parkTickets = new ArrayList<>();
+        this.visitors = new ArrayList<>();
+        this.employees = new ArrayList<>();
+        this.sections = new ArrayList<>();
+        this.rides = new ArrayList<>();
+        this.tickets = new ArrayList<>();
     }
 
     // Method to add a visitior
     public void addVisitor(Visitor v) {
+        // Check if the visitor object is not null and is not in the list
+        if (v != null && !visitors.contains(v)) {
+            visitors.add(v);
 
+            // Print confirmation that the visitor has been succefully added to the park
+            System.out.println("Visitor " + v.getName() + " has been added to the park.");
+        } else {
+            // If 'v' is null or visitor already in the list, print a message with the issue
+            System.out.println("Visitor already in the park or invalid.");
+        }
     }
 
     // Method to remove a visitor
@@ -48,7 +57,7 @@ public class Park {
     }
 
     // Method to assign an employee for a section
-    public void assignEmployee(Employee parkEmployees, Section parkSections) {
+    public void assignEmployee(Employee e, Section s) {
 
     }
 
@@ -58,7 +67,7 @@ public class Park {
     }
 
     // Method to manage tickets in the park
-    public void manageTickets(Ticket ticket) {
-        
+    public void manageTickets(Ticket t) {
+
     }
 }
