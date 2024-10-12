02de53df885ba6459134040e7ecc5d460844855e
kundyzs <serzhankyzykundyz@gmail.com>
2024-10-12 02:55:49 -0700

    Added constructor for BumperCars and called constructor of Ride

--- amusementpark/BumperCars.java
+++ amusementpark/BumperCars.java
@@ -2,5 +2,13 @@ package amusementpark;
 
 // BumperCars class extending the Ride class
 class BumperCars extends Ride {
-    
+    private int carCount; // Specific attribute for BumperCars - number of cars available
+
+    // Constructor to initialize BumperCars-specific and inherited attributes
+    public BumperCars(String rideName, String rideID, int rideCapacity, int rideDuration, int rideMinHeight, int rideMaxWeight, int carCount) {
+
+        // Calls the constructor of the parent class (Ride)
+        super(rideName, rideID, rideCapacity, rideDuration, rideMinHeight, rideMaxWeight);
+        this.carCount = carCount; // Set the number of cars for this bumper car ride
+    }
 }
