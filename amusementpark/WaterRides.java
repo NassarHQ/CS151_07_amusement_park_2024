8c173f199402479cf1af026ab70f9b379d070df6
kundyzs <serzhankyzykundyz@gmail.com>
2024-10-12 02:43:59 -0700

    Getter and setter for waterDepth

--- amusementpark/WaterRides.java
+++ amusementpark/WaterRides.java
@@ -11,4 +11,14 @@ class WaterRides extends Ride {
         super(rideName, rideID, rideCapacity, rideDuration, rideMinHeight, rideMaxWeight);
         this.waterDepth = waterDepth; // Set water depth for this water ride
     }
+
+    // Getter method for waterDepth
+    public double getWaterDepth() {
+        return waterDepth;
+    }
+
+    // Setter method for waterDepth
+    public void setWaterDepth(double waterDepth) {
+        this.waterDepth = waterDepth;
+    }
 }
