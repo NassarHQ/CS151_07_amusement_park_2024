c3238b5be0142ba6b0657889b916b0d614c8d188
kundyzs <serzhankyzykundyz@gmail.com>
2024-10-12 02:39:41 -0700

    Added constructor to initialize attributes

--- amusementpark/WaterRides.java
+++ amusementpark/WaterRides.java
@@ -2,5 +2,11 @@ package amusementpark;
 
 // WaterRides class extending the Ride class
 class WaterRides extends Ride {
-    
+    private double waterDepth; // Specific attribute for WaterRides
+
+    // Constructor to initialize WaterRides-specific and inherited attributes 
+    public WaterRides(String rideName, String rideID, int rideCapacity, int rideDuration, int rideMinHeight, int rideMaxWeight, double waterDepth) {
+
+        // Calls the constructor of the parent class (Ride)
+    }
 }
