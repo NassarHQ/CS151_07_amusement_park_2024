97b772814d94a7c941498a78acc59caf472a6147
Ryan Tran <tranryancollegeacc@gmail.com>
2024-10-13 12:41:54 -0700

    Implemented discountable by adjusting visitor class, ticket class, and created discountable file.

--- amusementpark/Ticket.java
+++ amusementpark/Ticket.java
@@ -1,6 +1,6 @@
 package amusementpark;
 
-public class Ticket {
+public class Ticket implements Discountable {
     private double ticketPrice;
     private String ticketID;
     private boolean isUsed; // Check if the ticket has been used
@@ -25,6 +25,29 @@ public class Ticket {
     public void setTicketPrice(double ticketPrice){
         this.ticketPrice = ticketPrice;
     }
+
+
+    @Override
+    public double applyDiscount(Visitor visitor){
+        String visitorCategory = visitor.getVisitorCategory();
+        double discount = 0.0;
+
+        switch (visitorCategory){
+            case "Child":
+                discount = 0.5;  //Children usually get the largest discounts
+                break;
+            case "Senior":
+                discount = 0.25;
+                break;
+            case "Adult":
+            default:
+                discount = 0.0; //No discount for regular adults
+                break;
+        }
+
+        return ticketPrice * (1 - discount);
+    }
+
 
     @Override
     public String toString() {
