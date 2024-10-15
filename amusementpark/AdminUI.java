c2c063803d47308adfc1665f34686fd554073298
kundyzs <serzhankyzykundyz@gmail.com>
2024-10-14 23:47:00 -0700

    Main function to control the UI

--- amusementpark/AdminUI.java
+++ amusementpark/AdminUI.java
@@ -14,4 +14,41 @@ public class AdminUI {
         System.out.println("5. Check Park metrics");
         System.out.println("Enter your choice: ");
     }
+
+    // Main function to control the UI
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            displayAdminMenu();
+            int choice = scanner.nextInt();
+
+            switch (choice){
+                case 1:
+                manageRides(scanner);
+                break;
+
+                case 2:
+                manageStores(scanner);
+                break;
+
+                case 3:
+                showFeedback(scanner);
+                break;
+
+                case 4:
+                showReports(scanner);
+                break;
+
+                case 5:
+                checkMetrics(scanner);
+
+                default:
+                    System.out.println("Invalid choice. Try again.");
+
+            }
+        }
+    }
+
+    
 }
