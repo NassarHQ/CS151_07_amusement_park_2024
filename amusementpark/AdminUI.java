ecd43bd97e01693141058a01a98757998211c4bb
kundyzs <serzhankyzykundyz@gmail.com>
2024-10-14 23:23:56 -0700

    Added main menu for Admin

--- amusementpark/AdminUI.java
+++ amusementpark/AdminUI.java
@@ -1,5 +1,17 @@
 package amusementpark;
 
+import java.util.Scanner;
+
 public class AdminUI {
     
+    // Main menu for Admin
+    public static void displayAdminMenu(){
+        System.out.println("Admin Menu:");
+        System.out.println("1. Manage Rides");
+        System.out.println("2. Manage Stores");
+        System.out.println("3. Show visitor feedback");
+        System.out.println("4. Show employee reports");
+        System.out.println("5. Check Park metrics");
+        System.out.println("Enter your choice: ");
+    }
 }
