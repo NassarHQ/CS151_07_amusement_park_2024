2021fd7e4ab9e1cb631a19638b1238e3cb969021
NassarHQ <mnassar.personal@gmail.com>
2024-10-16 11:19:31 -0700

    Minor Adjustment

--- amusementpark/AdminUI.java
+++ amusementpark/AdminUI.java
@@ -207,7 +207,7 @@ public class AdminUI {
             scanner.nextLine(); 
 
             System.out.println("Enter the store name: ");
-            String storeName = scanner.nextLine();
+            String storeName = scanner.nexx spotLine();
             ParkStore store = findStoreByName(park.getStoresList(), storeName);
 
             if (store == null) {
