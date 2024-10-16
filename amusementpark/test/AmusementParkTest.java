6c4a0041dc48e115fcfb42720cd641908ada684f
kundyzs <serzhankyzykundyz@gmail.com>
2024-10-16 16:38:59 -0700

    Added import statements to test class

---
+++ amusementpark/test/AmusementParkTest.java
@@ -0,0 +1,12 @@
+package amusementpark.test;
+
+import amusementpark.*;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class AmusementParkTest {
+    private Employee employee; 
+}
